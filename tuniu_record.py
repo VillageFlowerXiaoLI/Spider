8546d68ededbf77dcf0826e72c0b4bde01f2e9d9
DUXINLU <duxinlu81@live.com>
2017-05-04 19:55:53 +0800

    将途牛游记数据加入数据库

--- tuniu_record.py
+++ tuniu_record.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from ctrip_spider import get_travel
+from tuniu_spider import get_travels_url as get_travel
 import requests
 from random import randint
 from lxml import etree
@@ -11,11 +11,11 @@ db_path = '/Users/duxinlu/Desktop/dachuang2017/dachuang.db'
 req_headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
-    'Host': 'you.ctrip.com',
-    'Referer': 'http://you.ctrip.com/place/',
+    'Host': 'trips.tuniu.com',
+    'Referer': 'http://www.tuniu.com/',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36', }
@@ -40,27 +40,37 @@ data = get_html_root()
 
 def get_title():
     global data
-    title = data.xpath('//title/text()')[0].split('-')[0].strip()
+
+    title = data.xpath('//title/text()')[:-3]
 
     return title
 
 
-# 内容不好抓
 def get_content():
     global data
 
-    return ''
+    content = ''
+
+    content_info = data.xpath('//p[@class="section-des"]')
+
+    content_list = []
+    for i in content_info:
+        content_list.append(i.text[0])
+
+    content = '_'.join(content_list)
+
+    return content
 
 
-# 只取前五十张图片的url
 def get_img_urls():
     global data
 
-    img_info = data.xpath('//a[@share-pic="1"]')
+    img_info = data.xpath('//img[@style="display: inline;"]')
 
     img_list = []
-    for i in range(0, max(20, len(img_info))):
-        img_list.append(img_info[i].xpath('@href')[0])
+
+    for i in range(0, max(30, len(img_info))):
+        img_list.append(img_info[i].xpath('@data-src')[0])
 
     img_urls = '_'.join(img_list)
 
